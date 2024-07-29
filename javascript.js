dad9c54883d572d3fb5b7054454d79e781c1fddd
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-07-29 14:53:07 +0800

    Solved FizzBuzz

--- javascript.js
+++ javascript.js
@@ -1,18 +1,32 @@
-function add7(x){
-    return x +7;
-}   
+// function add7(x){
+//     return x +7;
+// }   
 
-function multiply(x, y){
-    return x * y;
+// function multiply(x, y){
+//     return x * y;
+// }
+
+// function capitalize(str){
+//     let strLowercase = str.toLowerCase();
+//     return strLowercase.charAt(0).toUpperCase() + strLowercase.slice(1);
+// }
+
+// function lastLetter(str) {
+//     return str.charAt(str.length - 1);
+// }
+
+// console.log(lastLetter("bAnanAlegs"));
+
+let userInput = new Number(prompt("Number"));
+
+for (let i = 0; i < userInput; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("FizzBuzz");
+    } else if (i % 5 == 0){
+        console.log("Buzz");
+    } else if (i % 3 == 0){
+        console.log("Fizz");
+    } else {
+        console.log(i)
+    }
 }
-
-function capitalize(str){
-    let strLowercase = str.toLowerCase();
-    return strLowercase.charAt(0).toUpperCase() + strLowercase.slice(1);
-}
-
-function lastLetter(str) {
-    return str.charAt(str.length - 1);
-}
-
-console.log(lastLetter("bAnanAlegs"));
